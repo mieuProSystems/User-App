118b0950b9771f030434c1767cfb49a853993d97
Subash-G <subashg@mieupro.com>
2019-11-12 18:40:25 +0530

    fix

--- Screen/AccountScreen.js
+++ Screen/AccountScreen.js
@@ -4,6 +4,9 @@ import {View,Text, Image} from 'react-native';
 
 import IPADDRESS from '../Components/serverip';
 
+import Icon from 'react-native-vector-icons/FontAwesome5Pro';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+
 
 
 
@@ -61,22 +64,35 @@ import IPADDRESS from '../Components/serverip';
           <View>
           {(this.state.data === false)?null:(<View style={{padding:10}}>
                     <View style={{borderRadius:30, flexDirection:'row', backgroundColor:'#ccc', marginTop:10, marginBottom:10, padding:10, borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid'}} >
-                    <View style={{width:100}}><Text style={{fontSize:20, color:'#000'}}>User-Id</Text></View>
+                    <View style={{width:140}}><Text style={{fontSize:20, color:'#000'}}>User-Id</Text></View>
                     <View><Text style={{fontSize:18}}>{this.state.data.userId}</Text></View>
                     </View>
 
                 <View style={{borderRadius:30,flexDirection:'row', backgroundColor:'#ccc', marginBottom:10, padding:10, borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid'}} >
-                <View style={{width:100}}><Text style={{fontSize:20, color:'#000'}}>E-mail</Text></View>
+                <View style={{width:140}}><Text style={{fontSize:20, color:'#000'}}>E-mail</Text></View>
                 <View style={{}}><Text style={{fontSize:18}}>{this.state.data.email}</Text></View>
                 </View>
                 
                 <View style={{borderRadius:30,flexDirection:'row', backgroundColor:'#ccc', marginBottom:10, padding:10, borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid'}} >
-                <View style={{width:100}}><Text style={{fontSize:20, color:'#000'}}>Password</Text></View>
+                <View style={{width:140}}><Text style={{fontSize:20, color:'#000'}}>Password</Text></View>
                 <View><Text style={{fontSize:18}}>********</Text></View>
                 </View>
 
+                <View style={{borderRadius:30,flexDirection:'row', backgroundColor:'#ccc', marginBottom:10, padding:10, borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid'}} >
+                <View style={{width:140}}><Text style={{fontSize:20, color:'#000'}}>Joined At</Text></View>
+                <View><Text style={{fontSize:18}}>{this.state.data.joinedAt}</Text></View>
+                </View>
+
+                <TouchableHighlight>
+            <View style={{borderRadius:30, backgroundColor:'#002aff', width:150,marginLeft:'auto',marginRight:'auto', padding:10, borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid'}}>
+              <Text style={{fontSize:22, color:'#fff', marginLeft:'auto', marginRight:'auto'}}>Logout</Text>
+            </View>
+          </TouchableHighlight>
+
               </View>)}
           </View>
+
+
         
         </View>  );
       }
