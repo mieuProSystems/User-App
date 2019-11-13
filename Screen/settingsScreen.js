d36ec60cbe572230b8227c52140a59cd1c0bd03e
Subash-G <subashg@mieupro.com>
2019-11-13 19:40:38 +0530

    Rate this app & Invite Friends completed
    Link must be changed

--- Screen/settingsScreen.js
+++ Screen/settingsScreen.js
@@ -1,58 +1,108 @@
 import React, { Component } from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text, Alert, Linking} from 'react-native';
 import { TouchableOpacity, TouchableHighlight } from 'react-native-gesture-handler';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Share from 'react-native-share';
+
+
+const shareOptions = {
+  title: 'Share via',
+  message: 'My App',
+  url: 'Downloadable link',
+  social: Share.Social.WHATSAPP,
+  
+};
+
+const options={
+  url:'http://www.google.com',
+  message:'Download This App',
+  title:'Download This App'
+};
+
 
 class SettingsScreen extends React.Component {
 
     componentWillMount (){
         console.log(this.props.location);
     }
+
+    shareToFriends = () => {
+
+      Share.open(options)
+    .then((res) => { console.log(res) })
+    .catch((err) => { err && console.log(err); });
+        
+      }
+
+
+      // Share.share(
+      //   {
+      //       message: "Link Test http:localhost:3000/  check it",
+      //       url:'www.google.com'
+      //   })
+      //   .then(result => console.log(result))
+      //   .catch(err => console.log(err));
+
+
+
+    rateThisApp=()=>{
+      Linking.openURL("market://details?id=com.google.android.apps.maps");
+    }
     
     render() {
+      
+    
       return (
         <View style={{ }}>
-          <View style={{flexDirection:'row'}}>
+        
 
-          <TouchableHighlight>
-            <View style={{borderRadius:15,width:150,  borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid', margin:20, backgroundColor:'#00a2ff', elevation:10}}>
-              <Text style={{fontSize:20, padding:20, textAlign:'center', marginTop:'auto', marginBottom:'auto'}}>Change Password</Text>
+          <TouchableOpacity>
+            <View style={{ padding:5, backgroundColor:'#fff', flexDirection:'row', borderBottomWidth:1, borderBottomColor:'#eee', borderStyle:'solid'}}>
+              <Icon style={{marginLeft:10, padding:10, marginTop:'auto', marginBottom:'auto'}} name='unlock' size={30} color="#00a2ff"/>
+              <Text style={{fontSize:20, padding:10, marginTop:'auto', marginBottom:'auto'}}>Change Password</Text>
             </View>
-            </TouchableHighlight>
+            </TouchableOpacity>
 
-            <TouchableHighlight onPress={()=>{this.props.navigation.navigate('Home')}}>
-          <View style={{borderRadius:15, width:150,  borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid', margin:20, backgroundColor:'#00a2ff', elevation:10}}>
-              <Text style={{fontSize:20, padding:20, textAlign:'center'}}>Help & Feedback</Text>
-            </View>
-            </TouchableHighlight>
-        </View>
-          <View style={{flexDirection:'row'}}>
-            <TouchableHighlight>
-            <View style={{borderRadius:15,width:150,  borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid', margin:20, backgroundColor:'#00a2ff', elevation:10}}>
-              <Text style={{fontSize:20, padding:20, textAlign:'center', marginTop:'auto', marginBottom:'auto'}}>Invite Friends</Text>
-            </View>
-            </TouchableHighlight>
+            <TouchableOpacity >
 
-            <TouchableHighlight>
-            <View style={{borderRadius:15,width:150,  borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid', margin:20, backgroundColor:'#00a2ff', elevation:10}}>
-              <Text style={{fontSize:20, padding:20, textAlign:'center', marginTop:'auto', marginBottom:'auto'}}>Rate This App</Text>
+            <View   style={{ padding:5, backgroundColor:'#ff', flexDirection:'row',borderBottomWidth:1, borderBottomColor:'#eee', borderStyle:'solid'}} >
+            <Icon style={{marginLeft:10, padding:10, marginTop:'auto', marginBottom:'auto'}} name='question-circle' size={30} color="#00a2ff"/>
+              <Text style={{marginLeft:1,fontSize:20, padding:10}} >Help & Feedback</Text>
+            </View>
+            </TouchableOpacity>
+        
+          <View>
+            <TouchableHighlight  onPress={this.handleOnPress} >
+            <View style={{ padding:5, backgroundColor:'#fff', flexDirection:'row',borderBottomWidth:1, borderBottomColor:'#eee', borderStyle:'solid'}} >
+            <Icon style={{marginLeft:10, padding:10, marginTop:'auto', marginBottom:'auto'}} name='users' size={24} color="#00a2ff"/>
+              <Text onPress={this.shareToFriends} style={{marginLeft:1,fontSize:20, padding:10, marginTop:'auto', marginBottom:'auto'}}>Invite Friends</Text>
             </View>
             </TouchableHighlight>
             </View>
-
-            <View style={{flexDirection:'row'}}>
-            <TouchableHighlight>
-            <View style={{borderRadius:15,width:150,  borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid', margin:20, backgroundColor:'#00a2ff', elevation:10}}>
-              <Text style={{fontSize:20, padding:20, textAlign:'center', marginTop:'auto', marginBottom:'auto'}}>About Us</Text>
-            </View>
-            </TouchableHighlight>
 
             <TouchableHighlight>
-            <View style={{borderRadius:15,width:150,  borderColor:'#00a2ff', borderWidth:1, borderStyle:'solid', margin:20, backgroundColor:'#00a2ff', elevation:10}}>
-              <Text style={{fontSize:20, padding:20, textAlign:'center', marginTop:'auto', marginBottom:'auto'}}>App Info</Text>
+
+            <View style={{ padding:5, backgroundColor:'#fff', flexDirection:'row',borderBottomWidth:1, borderBottomColor:'#eee', borderStyle:'solid'}}>
+            <Icon style={{marginLeft:10, padding:10, marginTop:'auto', marginBottom:'auto'}} name='star' size={28} color="#00a2ff"/>
+              <Text onPress={this.rateThisApp} style={{marginLeft:1,fontSize:20, padding:10, marginTop:'auto', marginBottom:'auto'}}>Rate This App</Text>
             </View>
             </TouchableHighlight>
+            
+
+            
+            <TouchableHighlight>
+            <View style={{ padding:5, backgroundColor:'#fff', flexDirection:'row',borderBottomWidth:1, borderBottomColor:'#eee', borderStyle:'solid', marginBottom:5}}>
+              <Icon style={{marginLeft:10, padding:10, marginTop:'auto', marginBottom:'auto'}} name='info-circle' size={30} color="#00a2ff"/>
+              
+              <Text style={{marginLeft:1,fontSize:20, padding:10, marginTop:'auto', marginBottom:'auto'}}>About Us</Text>
+              
             </View>
+            </TouchableHighlight>
+
+            
         </View>
       );
     }
